ae728b2df885ce4d556e5a2d8cfbc381dd77dffa
Nicholas O'Donnell <nicholas@nicholasodonnell.com>
2023-08-24 18:52:14 -0400

    lint

--- apps/client/src/components/movies.tsx
+++ apps/client/src/components/movies.tsx
@@ -65,7 +65,7 @@ export function MoviesSkeleton(): JSX.Element {
       {Array.from({ length: 100 }).map((_, index) => (
         <div
           key={index}
-          className="animate-pulse flex aspect-[2/3] h-full w-full flex-col items-center justify-center rounded-md border border-app-background-accent bg-app-background shadow"
+          className="flex aspect-[2/3] h-full w-full animate-pulse flex-col items-center justify-center rounded-md border border-app-background-accent bg-app-background shadow"
         />
       ))}
     </>
